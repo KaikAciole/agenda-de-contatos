c6aac8c6c50f70a6f02dbb996cf05b0111563135
KaikAciole <kaikaciole1.0@gmail.com>
2023-10-21 13:19:28 -0300

    correção

--- agenda-de-contatos/src/Agenda.java
+++ agenda-de-contatos/src/Agenda.java
@@ -10,4 +10,44 @@ public class Agenda {
         contatos.add(c);
     }
 
+    public ContatoBasico buscar(String nome){
+        for(int i=0; i< contatos.size(); i++){
+           ContatoBasico c = contatos.get(i);
+           if(c.getNome().equalsIgnoreCase(nome)){
+               return contatos.get(i);
+           }
+        }
+           return null;
+    }
+
+    public ContatoBasico buscar(int pos){
+        if(pos>=0 && pos< contatos.size()){
+            return contatos.get(pos);
+        }
+        return null;
+    }
+
+    public Contato[] buscarGeral(String palavraChave){
+        ArrayList<ContatoBasico> todos = new ArrayList<>();
+        for(int i=0; i<contatos.size(); i++){
+            ContatoBasico c = contatos.get(i);
+            if(c.getDados().contains(palavraChave)){
+                todos.add(c);
+            }
+        }
+        Contato[] vet = new Contato[todos.size()];
+
+        return (todos.toArray(vet));
+    }
+
+    public Contato[] busccarTodos(){
+        Contato[] vet = new Contato[contatos.size()];
+
+        return (contatos.toArray(vet));
+    }
+
+    public int getQuantidade(){
+        return contatos.size();
+    }
+
 }
